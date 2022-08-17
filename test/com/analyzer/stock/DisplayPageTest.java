70d6c633a2142d7cf3526977f706a79eb5ff249e
Mayuri Dalavai <mayuri.gj@gmail.com>
2022-08-17 12:47:09 -0700

    add test directory and Junit lib

---
+++ test/com/analyzer/stock/DisplayPageTest.java
@@ -0,0 +1,12 @@
+package com.analyzer.stock;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class DisplayPageTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
